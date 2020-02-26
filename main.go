1fbfabe9c3df71e002ed0722cfdd8783f75ec107
intern4 <intern4@intern-gold.local>
2020-02-26 11:56:07 -0500

    Changed main.go file to add routes - Added /tweed route and tweedHandler function - Added /svc route and svcHandler function

--- main.go
+++ main.go
@@ -11,10 +11,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Homepage Endpoint Hit")
 }
 
+func tweedHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Tweed Endpoint Hit")
+}
+
+func svcHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Service Catalog Endpoint Hit")
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", handler)
+	r.HandleFunc("/tweed", tweedHandler)
+	r.HandleFunc("/svc", svcHandler)
 
 	http.ListenAndServe(":80", r)
 }
