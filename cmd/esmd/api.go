1bd46ee53bfe09d0ccc00f686e4c35e34c451626
Michael Long <michaellong@starkandwayne.com>
2020-03-06 09:49:48 -0500

    Added more detail to the basic routes in the API

--- cmd/esmd/api.go
+++ cmd/esmd/api.go
@@ -20,10 +20,23 @@ func (api API) Run() *mux.Router {
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
+	//retrieve clouds
 	r.HandleFunc("/clouds", test_response)
-	r.HandleFunc("/register/{broker}", test_response)
+	//retrieve a specific cloud
+	r.HandleFunc("/clouds/{cloud}", test_response)
+	//retrieve all catalogs
 	r.HandleFunc("/catalog", test_response)
+	//provision a service
 	r.HandleFunc("/provision/{service}/{plan}", test_response)
+	//get an instance
+	r.HandleFunc("/instances/{instance}", test_response)
+	//deprovision an instance
 	r.HandleFunc("/deprovision/{instance}", test_response)
+	//bind an instance
+	r.HandleFunc("/bind/{instance}", test_response)
+	//retrieve binding
+	r.HandleFunc("/getbinding/{instance}", test_response)
+	//unbind an instance
+	r.HandleFunc("/unbind/{instance}", test_response)
 	return r
 }
