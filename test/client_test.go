ced551b270b31351cc82876cc76b1184184269d7
jgoldverg <jgoldverg@gmail.com>
2020-03-08 18:29:58 -0400

    Added more testing to Catalog

--- test/client_test.go
+++ test/client_test.go
@@ -10,7 +10,10 @@ import (
 )
 
 func TestCatalogNoAuth(t *testing.T) {
-	r := httptest.NewRequest("GET", util.GetTweedUrl()+"/b/catalog", nil)
+	r, err := http.NewRequest("GET", util.GetTweedUrl()+"/b/catalog", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(tweed.Catalog)
 	handler.ServeHTTP(rr, r)
@@ -20,12 +23,23 @@ func TestCatalogNoAuth(t *testing.T) {
 }
 
 func TestCatalogTestWithAuth(t *testing.T) {
-	r := httptest.NewRequest("GET", util.GetTweedUrl()+"/b/catalog", nil)
+	r, err := http.NewRequest("GET", util.GetTweedUrl()+"/b/catalog", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r.Header.Add("Content-Type", "application/json")
 	r.SetBasicAuth(util.GetUsername(), util.GetPassword())
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(tweed.Catalog)
 	handler.ServeHTTP(rr, r)
+	//confirms that the status does not get denied when credentials are passed to Catalog.
 	if status := rr.Code; status == http.StatusUnauthorized {
 		t.Errorf("Catalog did not Correctly handle user that is passing in crednetials. Got code %v", status)
+	} else if status != http.StatusOK {
+		t.Errorf("Got back a bad error form the tweed catalog: got %v want %v", status, http.StatusOK)
+	}
+	//if body is totally empty we did something wrong
+	if len(rr.Body.String()) <= 0 {
+		t.Errorf("The body was empty from the response of the Catalog handler")
 	}
 }
