b2fa4dc52db8e93a9d6315814dff5dd56a4619cc
jgoldverg <jgoldverg@gmail.com>
2020-03-13 11:07:34 -0400

    Refactored Tests for Bind()as they were not written correctly in the last commit

--- test/client_test.go
+++ test/client_test.go
@@ -1,11 +1,8 @@
 package test
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"testing"
 
 	"github.com/starkandwayne/external-service-marketplace/tweed"
@@ -41,70 +38,31 @@ func TestCatalogTestWithAuth(t *testing.T) {
 	}
 
 }
+
 func TestBindNoAuth(t *testing.T) {
-	r, _ := http.NewRequest("GET", util.GetTweedUrl()+"/b/catalog", nil)
-	r.Header.Add("Content-Type", "application/json")
-	handler := http.HandlerFunc(tweed.Bind)
+	id := ""
+	bid := ""
+	r, _ := http.NewRequest("PUT", util.GetTweedUrl()+"/b/instances/"+id+"/bindings/"+bid, nil)
 	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(tweed.Bind)
 	handler.ServeHTTP(rr, r)
-	if status := rr.Code; status != http.StatusUnauthorized {
-		t.Errorf("Catalog did not return %v when sent a request with an empty BasicAuth but got %v", http.StatusUnauthorized, http.StatusOK)
+	if rr.Code != http.StatusUnauthorized {
+		t.Errorf("Not passing crednetials allowed the Bind Function to fire off not good")
+	} else if rr.Code == http.StatusBadRequest {
+		t.Errorf("Error constructing your request double check that you made it right")
 	}
 }
-func TestBindAuthNoParam(t *testing.T) {
-	r, _ := http.NewRequest("GET", util.GetTweedUrl()+"/b/instances", nil)
-	r.Header.Add("Content-Type", "application/json")
-	r.SetBasicAuth(util.GetUsername(), util.GetPassword())
-	handler := http.HandlerFunc(tweed.Bind)
-	rr := httptest.NewRecorder()
-	handler.ServeHTTP(rr, r)
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("Passing no parameter for Bind still worked Not Godd check the client.go")
-	}
-}
-
-func TestBindNoBody(t *testing.T) {
-	r, err := http.NewRequest("GET", util.GetTweedUrl()+"/b/instances", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	r.Header.Add("Content-Type", "application/json")
-	r.URL.Query().Add("instance", "i-f5d7a4d99a0f49/tasks")
+func TestBindAuth(t *testing.T) {
+	id := ""
+	bid := ""
+	r, _ := http.NewRequest("PUT", util.GetTweedUrl()+"/b/instances/"+id+"/bindings/"+bid, nil)
 	r.SetBasicAuth(util.GetUsername(), util.GetPassword())
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(tweed.Bind)
 	handler.ServeHTTP(rr, r)
-	if status := rr.Code; status != http.StatusBadRequest {
-		out := "There was an error with the Bind() in the client---> code" + strconv.FormatInt(int64(status), 10)
-		t.Errorf(out)
-	}
-}
-
-func TestBindWithBody(t *testing.T) {
-	cmd := tweed.BindCommand{
-		ID:     "",
-		NoWait: false,
-		Args: struct {
-			ID string "positional-arg-name:\"instance\" required:\"true\""
-		}{
-			ID: "i-f5d7a4d99a0f49/tasks",
-		},
-	}
-	var body bytes.Buffer
-	b, _ := json.MarshalIndent(cmd, "", "  ")
-	body.Write(b)
-	r, err := http.NewRequest("GET", util.GetTweedUrl()+"/b/instances", &body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	r.URL.Query().Add("instance", "i-f5d7a4d99a0f49")
-	r.Header.Add("Content-Type", "application/json")
-	r.SetBasicAuth(util.GetUsername(), util.GetPassword())
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(tweed.Bind)
-	handler.ServeHTTP(rr, r)
-	if status := rr.Code; status != http.StatusOK {
-		out := "There was an error with the Bind() in the client---> code: " + strconv.FormatInt(int64(status), 10)
-		t.Errorf(out)
+	if rr.Code == http.StatusUnauthorized {
+		t.Errorf("Passed credentials to Bind() and ")
+	} else if rr.Code == http.StatusBadRequest {
+		t.Errorf("Error constructing your request double check that you made it right")
 	}
 }
